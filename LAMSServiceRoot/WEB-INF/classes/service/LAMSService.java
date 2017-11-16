f2cfac0cc228a18934e7c673e5825b1cb1d8c048
henrylkirk <henry@the-kirks.com>
2017-11-15 20:35:18 -0500

    Started adding functionality in service layer for adding and getting appointments

--- LAMSServiceRoot/WEB-INF/classes/service/LAMSService.java
+++ LAMSServiceRoot/WEB-INF/classes/service/LAMSService.java
@@ -12,35 +12,66 @@ public class LAMSService {
 
 	@WebMethod(operationName="Initialize")
    public String initialize(){
-         dbSingleton = DBSingleton.getInstance();
-         dbSingleton.db.initialLoad("LAMS");
-         return "Database Initialized";
+      dbSingleton = DBSingleton.getInstance();
+      dbSingleton.db.initialLoad("LAMS");
+      return "Database Initialized";
    }
    
    @WebMethod(operationName="GetAllAppointments")
-   public String GetAllAppointments(){
-      DBSingleton dbSingleton = DBSingleton.getInstance();
+   public String getAllAppointments(){
+      dbSingleton = DBSingleton.getInstance();
       String appointments = "";
       dbSingleton.db.initialLoad("LAMS");
       System.out.println("All appointments");
       List<Object> objs = dbSingleton.db.getData("Appointment", "");
       for (Object obj : objs){
-         appointments += obj;
+         appointments += obj.toString()+"\n";
       }
+      return appointments;
    }
-   
-   @WebMethod
-   public double calcCircle(double r){
-      return r*r*Math.PI;
+
+   @WebMethod(operationName="GetAppointment")
+   public String getAppointment(String appointmentID){
+      dbSingleton = DBSingleton.getInstance();
+      String appointment = "";
+      List<Object> objs = dbSingleton.db.getData("Appointment", "patientid='"+appointmentID+"'");
+      Patient patient = null;
+      Phlebotomist phleb = null;
+      PSC psc = null;
+      for (Object obj : objs){
+         appointment += obj.toString()+"\n";
+         // patient = ((Appointment)obj).getPatientid();
+         // phleb = ((Appointment)obj).getPhlebid();
+         // psc = ((Appointment)obj).getPscid();
+      }
+      return appointment;
    }
-   
-   public double calcRectangle(double w, double h){
-      return w*h;
-   }
-   
-   @WebMethod(exclude=true)
-   public String someMethod(){
-      return "";
+
+   @WebMethod(operationName="AddAppointment")
+   public String addAppointment(String xml){
+      String response = "";
+
+      System.out.println("^^^^^^^"+phleb.getId());
+      Appointment newAppt = new Appointment("800",java.sql.Date.valueOf("2009-09-01"),java.sql.Time.valueOf("10:15:00"));
+      //extra steps here due to persistence api and join, need to create objects in list
+      List<AppointmentLabTest> tests = new ArrayList<AppointmentLabTest>();
+      AppointmentLabTest test = new AppointmentLabTest("800","86900","292.9");
+      test.setDiagnosis((Diagnosis)dbSingleton.db.getData("Diagnosis", "code='292.9'").get(0));
+      test.setLabTest((LabTest)dbSingleton.db.getData("LabTest","id='86900'").get(0));
+      tests.add(test);
+      newAppt.setAppointmentLabTestCollection(tests);
+      newAppt.setPatientid(patient);
+      newAppt.setPhlebid(phleb);
+      newAppt.setPscid(psc);
+      boolean good = dbSingleton.db.addData(newAppt);
+      objs = dbSingleton.db.getData("Appointment", "");
+      for (Object obj : objs){
+         response += obj.toString()+"\n";
+      }
+
+      // bad appointment
+      // response = "<?xml version="1.0" encoding="UTF-8" standalone="no"?><AppointmentList><error>ERROR:Appointment is not available</error></AppointmentList>";
+      return response;
    }
 
 }
